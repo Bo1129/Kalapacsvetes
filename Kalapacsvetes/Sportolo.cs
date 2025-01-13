1b82eb1f43c6a6b50d38eb5570853d1be0fc33d9
Bo1129 <bohmb.22d@acsjszki.hu>
2025-01-13 09:38:21 +0100

    1

---
+++ Kalapacsvetes/Sportolo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalapacsvetes
+{
+    internal class Sportolo
+    {
+        public int helyezes { get; set; }
+        public int eredmeny { get; set; }
+        public string nev { get; set; }
+        public string orszag_kod { get; set; }
+        public string helyszin { get; set; }
+        public string datum { get; set; }
+
+        public Sportolo(string sor)
+        {
+            string[] s = sor.Split(';');
+
+            helyezes = int.Parse(s[0]);
+            eredmeny = int.Parse(s[1]);
+            nev = s[2];
+            orszag_kod = s[3];
+            helyszin = s[4];
+            datum = s[5];
+        }
+    }
+}
